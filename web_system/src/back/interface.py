58d9c346f99addf0e0c938b6419503a9c0f5d909
Christian Westbrook <christianwestbrook@live.com>
2020-05-07 21:26:29 -0600

    Added CORS to interface

--- web_system/src/back/interface.py
+++ web_system/src/back/interface.py
@@ -1,6 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_cors import CORS, cross_origin
 
-@app.route('/generate', methods=['POST'])
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS-HEADERS'] = 'Content-Type'
+
+@app.route('/generate', methods=['GET', 'POST'])
+@cross_origin()
 def hello_world():
     return 'Hello, World!'
