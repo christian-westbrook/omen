bfbf811165e2d21cc53f3d389a4835bfd2b5a5f4
Christian Westbrook <christianwestbrook@live.com>
2020-05-08 05:35:38 -0600

    Integrated extractor

--- web_system/src/engine/interface.py
+++ web_system/src/engine/interface.py
@@ -1,12 +1,28 @@
-from flask import Flask
+import os
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
-import extractor.singleExtract
+from extractor.singleExtract import extract
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'wav'}
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS-HEADERS'] = 'Content-Type'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/generate', methods=['GET', 'POST'])
 @cross_origin()
 def hello_world():
-    return 'Hello, World!'
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        frame = extract(filepath)
+        return frame.to_dict()
