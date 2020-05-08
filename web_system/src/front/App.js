b4e59055986457021bc6462820a41eaa9623a016
Christian Westbrook <christianwestbrook@live.com>
2020-05-07 19:11:30 -0600

    Added calls to back-end Node server

--- web_system/src/front/App.js
+++ web_system/src/front/App.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
 import Button from './modules/Button.js'
 import logo from './logo.jpeg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Open-source Music Recommendation Engine</p>
-        <Button enabled={true} label="Import Audio"  />
-        <br/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const input = useRef();
+    const [message, setMessage] = useState('Upload an audio file to generate recommendations');
+
+    const importAudio = () => {
+        let files = input.current.files;
+        let file  = files[files.length - 1];
+        
+        // Validation
+        if(file === undefined) {
+            setMessage('The file failed to upload. Please try again.');
+            return;
+        }
+
+        const data = new FormData();
+        data.append('file', file);
+        axios.post('http://' + document.domain + ':8000/upload', data, { })
+            .then(res => { setMessage(res.statusText);  });
+    }  
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Open-source Music Recommendation Engine</p>
+                <label>{message}</label>
+                <br/>
+                <input ref={input} type="file" onChange={importAudio} name="upload" />
+                <br/>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
